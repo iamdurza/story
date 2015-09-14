29a0c4615b0c26ddc36e0d54a64adb0f85d320d6
Jake <durza@skilstak.sh>
2015-09-14 19:00:47 -0400

    save

--- story/intro.py
+++ story/intro.py
@@ -1,4 +1,4 @@
-"""Start of the legendary quest of legendness
+"""Start of the legendary quest of legendnes
 
 Hello, i am your living bookworm guid to your legendary quest of legendness!
 
---
+++ story/start.py
@@ -0,0 +1,5 @@
+'''A bright flash of light and a sence of virtigo!
+
+
+    You wake up felling dizzy on a old dirt road. You see hoof prints leading to a dark forest. As it gets close to the forest, the road 
+    splits off, heading twords a village in the distance.
